e55da81f83021ad96ed65c92c901562daf0865f1
kauppine <24810630+kauppine@users.noreply.github.com>
2024-11-16 13:17:47 +0200

    Fix webhook resource name

--- sonarcloud/resource_webhook.go
+++ sonarcloud/resource_webhook.go
@@ -22,7 +22,7 @@ func NewWebhookResource() resource.Resource {
 }
 
 func (*WebhookResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_webhooks"
+	resp.TypeName = req.ProviderTypeName + "_webhook"
 }
 
 func (d *WebhookResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
